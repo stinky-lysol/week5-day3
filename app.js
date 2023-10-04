43a9d7dc0660782e823a281814202d73e565e083
stinky-lysol <143635048+stinky-lysol@users.noreply.github.com>
2023-10-03 18:29:26 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.navItem').each(function(){}).css({'color': 'white'});
+$('#nav').css({'background': '#145'});
+$('li:even').css({'background': '#ccc'});
+$('li:odd').css({'background': '#aaa'});
+$('label').text('Tage Cerny')
